4ba8e6e87f692bd26af4ab241d1a025429cf69b4
Waket Zheng <waketzheng@gmail.com>
2025-11-12 11:51:06 +0800

    tests: move gtid mode switch logic to a single function

--- conftest.py
+++ conftest.py
@@ -9,6 +9,54 @@ import pytest
 from tortoise.contrib.test import finalizer, initializer
 
 
+def _switch_gtid_mode() -> Callable[[], None] | None:
+    # SET gtid_mode to be OFF before testings
+    # And SET it ON after test before finalizer
+    from tortoise.contrib.test import _CONNECTIONS, _LOOP
+
+    conn = _CONNECTIONS.get("models")
+    assert conn is not None
+    run_coro = _LOOP.run_until_complete
+
+    async def get_var_value(statement: str) -> str:
+        result = await conn.execute_query_dict(statement)
+        return result[0]["Value"]
+
+    async def is_enforce_gtid() -> bool:
+        statement = "SHOW VARIABLES LIKE 'enforce_gtid_consistency';"
+        return (await get_var_value(statement)) == "ON"
+
+    async def is_gtid_mode_on() -> bool:
+        statement = "SHOW VARIABLES LIKE 'gtid_mode';"
+        return (await get_var_value(statement)) == "ON"
+
+    async def set_enforce_gtid_off() -> None:
+        statement = "SET GLOBAL enforce_gtid_consistency = OFF;"
+        if await is_gtid_mode_on():
+            off_mode = """
+            SET GLOBAL gtid_mode = ON_PERMISSIVE;
+            SET GLOBAL gtid_mode = OFF_PERMISSIVE;
+            SET GLOBAL gtid_mode = OFF;
+            """
+            statement = off_mode + statement
+        await conn.execute_script(statement)
+
+    async def set_enforce_gtid_on() -> None:
+        statement = "SET GLOBAL enforce_gtid_consistency = ON;"
+        if not (await is_gtid_mode_on()):
+            statement += """
+            SET GLOBAL gtid_mode = OFF_PERMISSIVE;
+            SET GLOBAL gtid_mode = ON_PERMISSIVE;
+            SET GLOBAL gtid_mode = ON;
+            """
+        await conn.execute_script(statement)
+
+    if run_coro(is_enforce_gtid()):
+        run_coro(set_enforce_gtid_off())
+        return functools.partial(run_coro, set_enforce_gtid_on())
+    return None
+
+
 @pytest.fixture(scope="session", autouse=True)
 def initialize_tests():
     # Reduce the default timeout for psycopg because the tests become very slow otherwise
@@ -24,50 +72,7 @@ def initialize_tests():
     rollback_sets: Callable[[], None] | None = None
     if db_url.startswith("mysql") and "storage_engine=MYISAM" in db_url:
         # Fixes "tortoise.exceptions.OperationalError: (1785, 'Statement violates GTID consistency: ...')" for `make test_mysql_myisam`
-        from tortoise.contrib.test import _CONNECTIONS, _LOOP
-
-        conn = _CONNECTIONS.get("models")
-        assert conn is not None
-        run_coro = _LOOP.run_until_complete
-
-        def get_var_value(statement: str) -> str:
-            result = run_coro(conn.execute_query_dict(statement))
-            return result[0]["Value"]
-
-        def is_enforce_gtid() -> bool:
-            statement = "SHOW VARIABLES LIKE 'enforce_gtid_consistency';"
-            return get_var_value(statement) == "ON"
-
-        def is_gtid_mode_on() -> bool:
-            statement = "SHOW VARIABLES LIKE 'gtid_mode';"
-            return get_var_value(statement) == "ON"
-
-        async def set_enforce_gtid_off(switch_mode: bool) -> None:
-            statement = "SET GLOBAL enforce_gtid_consistency = OFF;"
-            if switch_mode:
-                off_mode = """
-                SET GLOBAL gtid_mode = ON_PERMISSIVE;
-                SET GLOBAL gtid_mode = OFF_PERMISSIVE;
-                SET GLOBAL gtid_mode = OFF;
-                """
-                statement = off_mode + statement
-            await conn.execute_script(statement)
-
-        def set_enforce_gtid_on(switch_mode: bool) -> None:
-            statement = "SET GLOBAL enforce_gtid_consistency = ON;"
-            if switch_mode:
-                statement += """
-                SET GLOBAL gtid_mode = OFF_PERMISSIVE;
-                SET GLOBAL gtid_mode = ON_PERMISSIVE;
-                SET GLOBAL gtid_mode = ON;
-                """
-            run_coro(conn.execute_script(statement))
-
-        if is_enforce_gtid():
-            mode_on = is_gtid_mode_on()
-            run_coro(set_enforce_gtid_off(mode_on))
-            rollback_sets = functools.partial(set_enforce_gtid_on, mode_on)
-
+        rollback_sets = _switch_gtid_mode()
     try:
         yield
     finally:
