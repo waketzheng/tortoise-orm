a6316e1373a9331199a9441ba6ddced3fc87f570
Waket Zheng <waketzheng@gmail.com>
2026-05-15 01:59:39 +0800

    tests: reduce duplicated

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,6 +2,7 @@
 Tests for tortoise.config module - TortoiseConfig class.
 """
 
+import shutil
 from pathlib import Path
 
 import orjson
@@ -9,11 +10,27 @@ import pytest
 import yaml
 
 from tortoise.backends.base.config_generator import expand_db_url
-from tortoise.config import TortoiseConfig
+from tortoise.config import AppConfig, DBUrlConfig, TortoiseConfig
 from tortoise.exceptions import ConfigurationError
 
 
 class TestTortoiseConfig:
+    @pytest.fixture
+    def db_url(self) -> str:
+        return "sqlite://db.sqlite3"
+
+    @pytest.fixture
+    def simple_config(self, db_url: str) -> dict:
+        return {
+            "connections": {"default": db_url},
+            "apps": {
+                "app": {
+                    "models": ["app.models"],
+                    "default_connection": "default",
+                }
+            },
+        }
+
     @pytest.mark.parametrize(
         "config,msg",
         [
@@ -63,51 +80,74 @@ class TestTortoiseConfig:
         with pytest.raises(ConfigurationError, match=msg):
             TortoiseConfig.from_dict(config)  # type: ignore
 
-    def test_from_dict(self):
-        simple = {
-            "connections": {"default": "sqlite://db.sqlite3"},
-            "apps": {"app": {"models": ["app.models"]}},
-        }
-        assert TortoiseConfig.from_dict(simple) is not None
+    def test_from_dict(self, simple_config: dict):
+        assert TortoiseConfig.from_dict(simple_config) == TortoiseConfig(
+            connections={"default": DBUrlConfig(url="sqlite://db.sqlite3")},
+            apps={
+                "app": AppConfig(
+                    models=["app.models"], default_connection="default", migrations=None
+                )
+            },
+            routers=None,
+            use_tz=None,
+            timezone=None,
+        )
         full = {
             "connections": {
                 "default": "sqlite://db.sqlite3",
                 "second": "sqlite://db2.sqlite3",
             },
             "apps": {
-                "app1": {"models": ["app1.models"]},
+                "app1": {
+                    "models": ["app1.models"],
+                    "migrations": "app1.migrations",
+                },
                 "app2": {
                     "models": ["app2.models"],
                     "default_connection": "second",
+                    "migrations": "app2.migrations",
                 },
             },
             "routers": ["path.Router"],
             "use_tz": True,
             "timezone": "UTC",
         }
-        assert TortoiseConfig.from_dict(full) is not None
+        assert TortoiseConfig.from_dict(full) == TortoiseConfig(
+            connections={
+                "default": DBUrlConfig(url="sqlite://db.sqlite3"),
+                "second": DBUrlConfig(url="sqlite://db2.sqlite3"),
+            },
+            apps={
+                "app1": AppConfig(
+                    models=["app1.models"], default_connection=None, migrations="app1.migrations"
+                ),
+                "app2": AppConfig(
+                    models=["app2.models"],
+                    default_connection="second",
+                    migrations="app2.migrations",
+                ),
+            },
+            routers=["path.Router"],
+            use_tz=True,
+            timezone="UTC",
+        )
 
-    def test_from_config_file(self, tmp_path: Path):
-        simple = {
-            "connections": {"default": "sqlite://db.sqlite3"},
-            "apps": {"app": {"models": ["app.models"]}},
-        }
+    def test_from_config_file(self, tmp_path: Path, simple_config: dict):
         file = tmp_path / "tortoise_conf.json"
-        file.write_bytes(orjson.dumps(simple))
+        file.write_bytes(orjson.dumps(simple_config))
         filename: str = file.as_posix()
         assert (
             TortoiseConfig.from_config_file(file)
             == TortoiseConfig.from_config_file(filename)
-            == TortoiseConfig.from_dict(simple)
+            == TortoiseConfig.from_dict(simple_config)
             == TortoiseConfig.resolve_args(config_file=file)
         )
 
         yaml_file = file.with_suffix(".yml")
         with yaml_file.open("w") as f:
-            yaml.safe_dump(dict(simple), f, default_flow_style=False)
+            yaml.safe_dump(simple_config, f, default_flow_style=False)
         yaml_file_2 = file.with_suffix(".yaml")
-        with yaml_file_2.open("w") as f2:
-            yaml.safe_dump(simple, f2, default_flow_style=False)
+        shutil.copy(yaml_file, yaml_file_2)
         assert (
             TortoiseConfig.from_config_file(yaml_file)
             == TortoiseConfig.from_config_file(str(yaml_file))
@@ -116,21 +156,11 @@ class TestTortoiseConfig:
             == TortoiseConfig.resolve_args(config_file=yaml_file)
         )
 
-    def test_from_db_url_and_modules(self):
-        simple = {
-            "connections": {"default": "sqlite://db.sqlite3"},
-            "apps": {
-                "app": {
-                    "models": ["app.models"],
-                    "default_connection": "default",
-                }
-            },
-        }
-        db_url = simple["connections"]["default"]
-        modules = {"app": simple["apps"]["app"]["models"]}
+    def test_from_db_url_and_modules(self, simple_config: dict, db_url: str):
+        modules = {"app": simple_config["apps"]["app"]["models"]}
         typed_config = TortoiseConfig.from_db_url_and_modules(db_url, modules)
         assert typed_config == TortoiseConfig.resolve_args(db_url=db_url, modules=modules)
-        assert typed_config.apps == TortoiseConfig.from_dict(simple).apps
+        assert typed_config.apps == TortoiseConfig.from_dict(simple_config).apps
 
     @pytest.mark.parametrize(
         "config,msg",
@@ -150,20 +180,10 @@ class TestTortoiseConfig:
         with pytest.raises(ConfigurationError, match=msg):
             TortoiseConfig.resolve_args(**config)
 
-    def test_resolve_args(self, tmp_path: Path):
-        db_url = "sqlite://db.sqlite3"
-        config = {
-            "connections": {"default": db_url},
-            "apps": {
-                "app": {
-                    "models": ["app.models"],
-                    "default_connection": "default",
-                }
-            },
-        }
+    def test_resolve_args(self, tmp_path: Path, db_url: str, simple_config: dict):
         config_file = tmp_path / "config.json"
-        config_file.write_bytes(orjson.dumps(config))
-        typed_config = TortoiseConfig.resolve_args(config)
+        config_file.write_bytes(orjson.dumps(simple_config))
+        typed_config = TortoiseConfig.resolve_args(simple_config)
         assert typed_config == TortoiseConfig.resolve_args(config_file=config_file)
 
         typed_config_2 = TortoiseConfig.resolve_args(db_url=db_url, modules={"app": ["app.models"]})
